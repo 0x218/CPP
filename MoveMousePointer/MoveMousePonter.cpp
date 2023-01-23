8ff4e2c73f68398560483f193b8c8b94dfc4f4b9
Renjith SA <sa.renjith@gmail.com>
2023-01-23 14:01:50 +0000

    Update MoveMousePonter.cpp

--- MoveMousePointer/MoveMousePonter.cpp
+++ MoveMousePointer/MoveMousePonter.cpp
@@ -2,42 +2,43 @@
 // A fun Win32 console program.
 // Hide console window and move mouse pointer at random postion.
 
+// MoveMousePointer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 #include <random>
 #include <Windows.h>
 
 int main()
 {
-    int iW, iH = 0;
-    
     double screenWidth = ::GetSystemMetrics(SM_CXSCREEN) - 1;
     double screenHeight = ::GetSystemMetrics(SM_CYSCREEN) - 1;
     
-    ////Resize console window
-    //RECT rect;
-    //HWND hWndConsole = GetConsoleWindow(); //handle to the console window
-    //GetWindowRect(hWndConsole, &rect);
-    //MoveWindow(hWndConsole, rect.left, rect.top, 1000, 300, TRUE); // new dimension for the Console
-    
-    ::ShowWindow(::GetConsoleWindow(), SW_HIDE); //hide console
+    HWND hWndConsole = GetConsoleWindow();
+    RECT rect;
+    GetWindowRect(hWndConsole, &rect);
+    MoveWindow(hWndConsole, rect.left, rect.top, 1000, 300, TRUE); // new dimension
+    int iW, iH = 0;
+
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 
     while (TRUE) {
-        std::random_device rand;
-        std::mt19937 generator(rand()); //get a random number and seed the generator
+        std::random_device rand; // obtain a random number from hardware
+        std::mt19937 generator(rand()); // seed the generator
 
-        //Define a range
+        // define the range
         std::uniform_int_distribution<> distScreenWidth(10, screenWidth); 
         std::uniform_int_distribution<> distScreenHeight(10, screenHeight); 
-        
-        //Generate random numbers within the range
-        iW = distScreenWidth(generator); 
+
+        iW = distScreenWidth(generator); // generate numbers
         iH = distScreenHeight(generator);
-        //std::cout << "Mouse position (R x C) " << iW <<  " x " << iH << std::endl;
-        
-        //move the cusor
+        std::cout << "Mouse position (R x C) " << iW <<  " x " << iH << std::endl;
         SetCursorPos(iW, iH);
-        Sleep(3000);
-        //system("cls");
+        //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+    
+        Sleep(500000);
+        system("cls");
     }
 
     return 0;
